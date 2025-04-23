4d724fc61dc28ff3dd576832f21f1a8ef4db8d18
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2025-04-23 09:33:06 +0300

    init

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,59 @@
-console.log("ho-ho")
+const swiper = new Swiper(".hero-swiper", {
+  loop: true,
+  centeredSlides: true,
+  speed: 1200,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      opacity: 0,
+      translate: ["-100%", 0, -400],
+      scale: 0.9,
+    },
+    next: {
+      opacity: 0,
+      translate: ["100%", 0, -400],
+      scale: 0.9,
+    },
+  },
+});
+
+const swiperReports= new Swiper('.reports__swiper', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+const headerNavLinks = document.querySelectorAll('.header-nav-item a');
+const burgerMenu = document.getElementById('burger-menu');
+const navWrapper = document.querySelector('.header-nav-wrapper');
+
+burgerMenu.addEventListener('click', function() {
+  navWrapper.classList.toggle('open');
+});
+  
+headerNavLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navWrapper.classList.remove('open');
+  });
+});
+  
